9a43938b2aa1eb59876a2a62bd2bf702d8ef8a82
nagssh <126979843+nagssh@users.noreply.github.com>
2023-06-02 22:08:24 +0530

    car animation using HTML,CSS and javascript

---
+++ car animation/car.js
@@ -0,0 +1,10 @@
+// Add this JavaScript code to your car.js file or within a script tag in your HTML file
+
+// Wait for the DOM to load
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Get the car element
+    const carElement = document.querySelector('.car');
+
+    // Start the animation by adding the 'running' class to the car element
+    carElement.classList.add('running');
+});
